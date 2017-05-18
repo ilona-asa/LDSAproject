e113d79145408575d5e60695d9e6b522b0a02e6f
jonasmattis <jonas_mattisson@hotmail.com>
2017-05-18 12:15:29 +0200

    A less rough overview of the mapper, not yet tested however (and probably doesn't work as we intend)

--- mapper.py
+++ mapper.py
@@ -6,22 +6,31 @@ import sys
 # each line in each file
 for line in sys.stdin:
 	
+	Date = ""
+	From = ""
+	
 	#So that we only read header
 	while line != "\n":
 		
-		# remove leading and trailing whitespace
-		#line = line.strip()
+		#remove leading and trailing whitespace
+		line = line.strip()
 		
 		# split the line into words
 		words = line.split()
     
-		if "From:" in words:
+		if "Date:" == words[0]:
+			
+			#Pick second word
+			Date = words[1]
+	
+		if "From:" == words[0]:
+			
+			#Pick second word
+			From = words[1]
+			
+		if Date != "" and From != "":
+			print '%s\t%s' % ([From, Date], 1)
+			
 		
-			#PIC SECOND WORD IN WORDS
-			#print '%s\t%s' % (word, 1)
 			
-	    if "Date:" in words:
-			
-			#PICK SECOND WORD IN WORDS
-			#print '%s\t%s' % (word, 1)	
 		
