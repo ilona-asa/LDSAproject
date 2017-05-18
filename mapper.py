7fb9feee172656aad51a38cffda67c76d003cf74
karl <leife_91@hotmail.com>
2017-05-18 12:46:12 +0200

    improved mapper.py

--- mapper.py
+++ mapper.py
@@ -5,14 +5,27 @@ import sys
 # input comes from STDIN (standard input)
 for line in sys.stdin:
     # remove leading and trailing whitespace
-    line = line.strip()
-    # split the line into words
-    words = line.split()
-    # increase counters
-    for word in words:
-        # write the results to STDOUT (standard output);
-        # what we output here will be the input for the
-        # Reduce step, i.e. the input for reducer.py
-        #
-        # tab-delimited; the trivial word count is 1
-        print '%s\t%s' % (word, 1)
+#    while line != "\n":
+
+        line = line.strip()
+        # split the line into words
+        words = line.split()
+        #print words[0]
+        if 'Date:' in words[0]:
+                date_temp = words[1]
+                date = date_temp[:-1]
+                print date
+        
+        if 'From:' in words[0]:
+                sender =  words[1]
+                break
+        
+
+        # increase counters
+        #for word in words:
+            # write the results to STDOUT (standard output);
+            # what we output here will be the input for the
+            # Reduce step, i.e. the input for reducer.py
+            #
+            # tab-delimited; the trivial word count is 1
+        print '%s\t%s' % ((sender,date), 1)
