6048fab00b999463261c5f3296a42cae1f02e4c9
ilona-asa <asailona@gmail.com>
2017-05-19 01:19:04 +0200

    resolve conflict on mapper.py

--- mapper.py
+++ mapper.py
@@ -5,7 +5,6 @@ import sys
 # input comes from STDIN (standard input)
 # each line in each file
 for line in sys.stdin:
-<<<<<<< HEAD
     # remove leading and trailing whitespace
 #    while line != "\n":
 
@@ -31,34 +30,3 @@ for line in sys.stdin:
             #
             # tab-delimited; the trivial word count is 1
         print '%s\t%s' % ((sender,date), 1)
-=======
-	
-	Date = ""
-	From = ""
-	
-	#So that we only read header
-	while line != "\n":
-		
-		#remove leading and trailing whitespace
-		line = line.strip()
-		
-		# split the line into words
-		words = line.split()
-    
-		if "Date:" == words[0]:
-			
-			#Pick second word
-			Date = words[1]
-	
-		if "From:" == words[0]:
-			
-			#Pick second word
-			From = words[1]
-			
-		if Date != "" and From != "":
-			print '%s\t%s' % ([From, Date], 1)
-			
-		
-			
-		
->>>>>>> be5fae70cd41db3b0b81fc48d2075ea36063bdb0
