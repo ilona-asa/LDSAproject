7203a2502723af2483410c3fdff4b41c289b74dc
jonasmattis <jonas_mattisson@hotmail.com>
2017-05-26 12:29:52 +0200

    Email concat script. Got glob to work, but they now can't seem to find the files, even with correct path (atleast what it looks like to me).

--- concatEmails.py
+++ concatEmails.py
@@ -3,6 +3,8 @@
 import os
 import glob
 
+os.sep = '/'
+
 #IMPORTANT: Root directory to the mail below
 rootdir ='C:/Users/Jonas/Documents/UU/LDSA/projectemails/testmail'
 
@@ -14,16 +16,18 @@ for user in os.listdir(rootdir):
 		if sent == 'sent_items':
 			
 			#Picks all the files
-			files = glob.glob(rootdir+'/'+user+'/'+sent+'/*')
-			
-			print(files)
-			
+			files = glob.glob(rootdir+'/'+user+'/'+sent+'/*.')
 			#Open the file to write in, might have to fix path!
 			with open(rootdir+'/'+"concatEmails"+str(i)+".txt", "wb") as outfile:
 			
 				#Opens the files in the current folder
 				for f in files:
 				
+					#Converts to correct file path
+					fbase = os.path.basename(f)
+					f = rootdir+'/'+user+'/'+sent+'/'+fbase
+					print(f)
+					
 					#Open the email and write it to the current concat file
 					with open(f, "rb") as infile:
 					
@@ -35,7 +39,10 @@ for user in os.listdir(rootdir):
 					if file_info.st_size >= 67108864:
 						#Otherwise create a new outfile
 						i += 1
-						outfile.close
+
+						#Seem not to be needed
+						#outfile.close
+						
 						outfile = open(rootdir+'/'+"concatEmails"+str(i)+".txt", "wb")
 						
 					#If we then dare to remove the files afterwards
