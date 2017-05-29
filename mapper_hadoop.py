64399b23250f7c0f8cd7f6a71ee45c6fca5fe275
ilona-asa <asailona@gmail.com>
2017-05-29 13:47:07 +0200

    the mapper for running in hadoop env

---
+++ mapper_hadoop.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import sys
+
+for line in sys.stdin:
+        # remove leading and trailing whitespace
+        line = line.strip()
+        words = line.split()
+        if words != []:
+                if 'Message-ID:' in words[0]:
+                        DatePossible = True
+                        FromPossible = True
+                        sender = None
+                        date = None
+
+                if 'Date:' in words[0] and DatePossible:
+                        date_temp = words[1]
+                        date = date_temp[:-1]
+                        DatePossible = False
+
+                if 'From:' in words[0] and FromPossible:
+                        sender =  words[1]
+                        FromPossible = False
+                        if 'enron' in words[1]:
+                                print '%s\t%s' % ((sender,date), 1)
+                                #print '%s\t%s' % ('emailCount', 1)
+			#f.close()
